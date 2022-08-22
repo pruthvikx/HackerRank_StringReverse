24c4fa96944e4f0c99dd5c3133d7319f40023388
Pruthvi Kolhe <65055563+pruthvikx@users.noreply.github.com>
2022-08-22 15:39:51 +0530

    string palindrome hackerrank in java

---
+++ src/Solution.java
@@ -0,0 +1,19 @@
+import java.util.Scanner;
+
+public class Solution {
+    public static void main(String[] args) {
+
+        Scanner sc=new Scanner(System.in);
+        String A=sc.next();
+        String rev = "";
+
+        for (int i = A.length()-1;i>=0;i--){
+            rev = rev + A.charAt(i);
+        }
+        if (A.equals(rev)){
+            System.out.println("Yes");
+        }else {
+            System.out.println("No");
+        }
+    }
+}
